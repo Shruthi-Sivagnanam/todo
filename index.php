1561270be71df5237c5c8a2221189d55e78af50c
Shruthi-Sivagnanam <shruthi.11012003@gmail.com>
2023-06-27 12:31:25 +0530

    Frontend setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+    include("./components/header.php");
+?>
+
+<div class="container">
+    <div class="row align-items-center text-center"  style="min-height: 80vh">
+        <div class="col-md-12">
+            <h1 class="mb-3">Plan Your Activity with Us ....</h1>
+            <a 
+                href="./pages/signup.php" 
+                class="btn btn-outline-primary btn-md" 
+                role="button" aria-pressed="true"
+                >
+                Get Started ->
+            </a>
+
+        </div>
+    </div>
+</div>
